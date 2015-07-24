608665cffe96f98e878a41bc86bba3fba90b51f0
Andrew Kreps <andrew.kreps@gmail.com>
2015-07-24 08:54:20 -0700

    First working test, yay!

--- lib/lita/handlers/onewheel_karma.rb
+++ lib/lita/handlers/onewheel_karma.rb
@@ -1,6 +1,21 @@
 module Lita
   module Handlers
     class OnewheelKarma < Handler
+      route /(.+)\+\+$/, :add_one_karma,
+            help: 'object++: Add one karma to [object].'
+
+      def add_one_karma(response)
+        karma_object = response.matches[0][0]
+        karma = redis.get(karma_object)
+        if karma.nil?
+          karma = 1
+        else
+          karma += 1
+        end
+
+        redis.set(karma_object, karma)
+        response.reply "#{karma_object} has #{karma} karma!"
+      end
     end
 
     Lita.register_handler(OnewheelKarma)
--- spec/lita/handlers/onewheel_karma_spec.rb
+++ spec/lita/handlers/onewheel_karma_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Lita::Handlers::OnewheelKarma, lita_handler: true do
   it 'will add one karma' do
-    send_command 'karma a'
+    send_message 'a++'
     expect(replies.last).to eq 'a has 1 karma!'
   end
 end
