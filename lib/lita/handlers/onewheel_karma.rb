e945b0c6ce972aa899374893d52617036f4b6e87
Andrew Kreps <andrew.kreps@gmail.com>
2015-07-24 16:08:51 -0700

    Multipliers!

--- lib/lita/handlers/onewheel_karma.rb
+++ lib/lita/handlers/onewheel_karma.rb
@@ -1,9 +1,11 @@
 module Lita
   module Handlers
     class OnewheelKarma < Handler
-      route /(.+)\+\+/, :add_one_karma,
+      route /(.+)\s*\+\+/, :add_one_karma,
             help: 'object++: Add one karma to [object].'
-      route /(.+)--/, :remove_one_karma,
+      route /(.+)\s*--/, :remove_one_karma,
+            help: 'object++: Remove one karma from [object].'
+      route /(.+)\s*\*=\s*(\d+)/, :multiply_karma,
             help: 'object++: Remove one karma from [object].'
 
       def add_one_karma(response)
@@ -18,6 +20,13 @@ module Lita
         response.reply reply_with_karma(karma_object, karma)
       end
 
+      def multiply_karma(response)
+        karma_object = response.matches[0][0]
+        multiplier = response.matches[0][1]
+        karma = find_and_set_karma(karma_object, multiplier, true)
+        response.reply reply_with_karma(karma_object, karma)
+      end
+
       def reply_with_karma(karma_object, karma)
         if karma >= 0
           "#{karma_object} has #{karma} karma!"
@@ -27,9 +36,15 @@ module Lita
       end
 
       # Find the karma object from redis and increment appropriately.
-      def find_and_set_karma(karma_object, increment_value = 1)
+      def find_and_set_karma(karma_object, increment_value = 1, multiply = false)
         karma = redis.get(karma_object).to_i
-        karma += increment_value
+
+        if multiply
+          karma *= increment_value.to_i
+        else
+          karma += increment_value
+        end
+
         redis.set(karma_object, karma)
         karma
       end
--- spec/lita/handlers/onewheel_karma_spec.rb
+++ spec/lita/handlers/onewheel_karma_spec.rb
@@ -1,17 +1,58 @@
 require 'spec_helper'
 
 describe Lita::Handlers::OnewheelKarma, lita_handler: true do
+  NEGATIVE_MESSAGE = 'a has -💩 karma!'
+
   it 'will add one karma' do
     send_message 'a++'
     expect(replies.last).to eq 'a has 1 karma!'
   end
+
   it 'will add two karma' do
     send_message 'a++'
     send_message 'a++'
     expect(replies.last).to eq 'a has 2 karma!'
   end
+
   it 'will remove one karma' do
     send_message 'a--'
-    expect(replies.last).to eq 'a has -💩 karma!'
+    expect(replies.last).to eq NEGATIVE_MESSAGE
+  end
+
+  it 'will remove lotsa karma' do
+    send_message 'a--'
+    send_message 'a--'
+    send_message 'a--'
+    send_message 'a--'
+    send_message 'a--'
+    send_message 'a--'
+    expect(replies.last).to eq NEGATIVE_MESSAGE
+  end
+
+  it 'will multiply one karma' do
+    send_message 'a++'
+    send_message 'a*=2'
+    expect(replies.last).to eq 'a has 2 karma!'
+  end
+
+  it 'will multiply many karma' do
+    send_message 'a++'
+    send_message 'a++'
+    send_message 'a++'
+    send_message 'a++'
+    send_message 'a++'
+    send_message 'a*=5'
+    expect(replies.last).to eq 'a has 25 karma!'
+  end
+
+  it 'will multiply many negative karma' do
+    send_message 'a--'
+    send_message 'a--'
+    send_message 'a--'
+    send_message 'a--'
+    send_message 'a--'
+    send_message 'a*=5'
+    # -25
+    expect(replies.last).to eq NEGATIVE_MESSAGE
   end
 end
